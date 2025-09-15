a84753732c5d3a2fffddc381c8eb9172591d9ab0
Андрей Алисов <andrejalisov7@gmail.com>
2025-09-16 02:06:52 +0300

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,47 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+main()
+{    
+	setlocale(LC_CTYPE, "RUS");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                                                                                          ");
+	puts("                                      _______         _______                                             ");
+	puts("                                     |       \       /       |                                            ");
+	puts("                                     |        \     /        |                                            ");
+	puts("                                     |         \   /         |                                            ");
+	puts("                                     |_________ \ / _________|                                            ");
+	puts("                                     __________ /|\__________                                             ");
+	puts("                                    /          / | \         \                                            ");
+	puts("                                   /          /  |  \         \                                           ");
+	puts("                                  /          /   |   \         \                                          ");
+	puts("                                 /          /    |    \         \                                         ");
+	puts("                                /           |    |    |          \                                        ");
+	puts("                                \           |    |    |          /                                        ");
+	puts("                                 \          |    |    |         /                                         ");
+	puts("                                  \         |_________|        /                                          ");
+	puts("                                   \_______/   |  |   \_______/                                           ");
+	puts("                                      \ \      |  |      / /                                              ");
+	puts("                                       \ \     |  |     / /                                               ");
+	puts("                                        \ \    |  |    / /                                                ");
+	puts("                                         \ \   |  |   / /                                                 ");
+	puts("                                          \ \  |  |  / /                                                  ");
+	puts("                                           \ \ |  | / /                                                   ");
+	puts("                                            \ \|  |/ /                                                    ");
+	puts("                                             \ |  | /                                                     ");
+	puts("										        \|  |/                                                      ");
+	puts("                                               |  |                                                       ");
+	puts("												 |__|                                                       ");
+}
